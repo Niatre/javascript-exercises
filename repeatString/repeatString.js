61242b4a61b554830bc2430f139894f5efdfe24d
Niatre <niatree9999@gmail.com>
2019-09-10 17:20:03 +0200

    finish repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+    let repeatedString = "";
+    if (times < 0) {
+        return "ERROR"
+    }
+    while (times > 0) {
+      repeatedString = repeatedString + str;
+      times--;
+    }
+    return repeatedString;
 }
-
 module.exports = repeatString
