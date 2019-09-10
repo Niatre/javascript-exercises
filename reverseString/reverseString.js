6edc69d89d928a80f2f6f863d88901867a784a99
Niatre <niatree9999@gmail.com>
2019-09-10 17:25:58 +0200

    finished reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let split = str.split("");
+reversedArr = split.reverse();
+reversedString =reversedArr.join("");
+return reversedString
 }
 
 module.exports = reverseString
